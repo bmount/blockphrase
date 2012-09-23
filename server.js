d7cc4f70a588c99154961dcfb3f7e4cd4833cc4f
Brian Mount <bvmount@gmail.com>
2012-09-22 18:42:07 -0700

    server w get foreign image data, flood fill init

---
+++ server.js
@@ -0,0 +1,23 @@
+var taco = require('tako')
+  , request = require('request')
+  , path = require('path')
+  , filed = require('filed')
+  , app = taco()
+
+var tileset = 'toner-background'
+var tileset = 'toner-background'
+
+// getImageData has separate same origin policy,
+// proxy for pixel manipulation:
+
+app.route('/' + tileset + '/*', function (req, resp) {
+  request("http://tile.stamen.com"+req.url).pipe(resp)
+})
+
+app.route('/').files(path.join(__dirname, 'index.html'));
+
+app.route('/lib/*').files(path.join(__dirname, 'lib'));
+
+app.route('devtiles/*').files(path.join(__dirname, 'localtilecache'));
+
+app.httpServer.listen(8002)
